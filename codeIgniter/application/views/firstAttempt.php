ffec874d002c12b5946cc325ed2f3bf83a429a81
Dheeraj Dayakaran <dheerajdayakaran95@gmail.com>
2021-03-09 13:44:31 +0530

    created controller and method, multiple view loads, helpers for linking css-js

---
+++ codeIgniter/application/views/firstAttempt.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css')?>">
+        <title>
+        Dashboard
+        </title>
+    </head>
+    <h1>Welcome to Dashboard</h1>
+    <script src="<?php echo base_url('assets/js/bootstrap.js')?>"></script>
+</html>
